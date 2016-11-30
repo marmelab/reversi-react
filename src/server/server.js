82ea6a993084a5583d62b20a92567cb1906c37f0
Julien <julien@marmelab.com>
2016-11-30 15:59:49 +0100

    PR Fix, CamelCase => camelCase and fixed deps ver

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
-import AppMiddleware from './middleware/app';
-import ApiMiddleware from './middleware/api';
+import appMiddleware from './middleware/app';
+import apiMiddleware from './middleware/api';
 
 const app = express();
 
@@ -9,8 +9,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.use('/public', express.static(path.join(__dirname, '../../public')));
-app.use('/api', ApiMiddleware);
-app.use(AppMiddleware);
+app.use('/api', apiMiddleware);
+app.use(appMiddleware);
 
 const PORT = process.env.PORT || 8080;
 
