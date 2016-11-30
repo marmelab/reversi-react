53adea85b77056c29e45a481754c953d8fcfceb5
Julien <julien@marmelab.com>
2016-11-30 14:28:49 +0100

    Rename some files (reducer, action, store) + move StyleProvider + add hashGenerator in utils

--- src/shared/app/reducers/game.js
+++ src/shared/app/reducers/game.js
@@ -1,31 +1,9 @@
-import {
-    PLACE_CELL_CHANGE,
-    START_NEW_GAME,
-} from '../actions/GameActions';
+import { RECEIVE_GAME } from '../actions/Game';
 
-import { create as createPlayer } from '../../reversi/player/Player';
-import { create as createGame, playCellChange, tryPlayerSwitch } from '../../reversi/game/Game';
-import { TYPE_WHITE, TYPE_BLACK } from '../../reversi/cell/Cell';
-
-const initGame = (againstComputer) => {
-    if (againstComputer) {
-        return createGame([
-            createPlayer('You', TYPE_BLACK),
-            createPlayer('Computer', TYPE_WHITE, false),
-        ]);
-    }
-    return createGame([
-        createPlayer('Player 1', TYPE_BLACK),
-        createPlayer('Player 2', TYPE_WHITE),
-    ]);
-};
-
-export default (game = initGame(false), action = {}) => {
+export default (game = null, action = {}) => {
     switch (action.type) {
-    case PLACE_CELL_CHANGE:
-        return tryPlayerSwitch(playCellChange(action.cellChange, game));
-    case START_NEW_GAME:
-        return initGame(action.againstComputer);
+    case RECEIVE_GAME:
+        return action.game;
     default:
         return game;
     }
